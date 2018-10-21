8f55b1240503ebb5f4fccc0c5ac975154b4dc3b8
hanjoo <johanjoo@naver.com>
2018-10-21 09:15:56 -0700

    키보드 디바이스 업그레이드

--- 02.Kernel64/Source/Main.c
+++ 02.Kernel64/Source/Main.c
@@ -14,123 +14,120 @@ void Main( void )
 	BYTE bFlags;
 	BYTE bTemp;
 	int i=0;
+	KEYDATA stData;
 
 
-    kPrintString( 0, 11, "Switch To IA-32e Mode Success~!!" );
-    kPrintString( 0, 12, "IA-32e C Language Kernel Start..............[Pass]" );
+	kPrintString( 0, 11, "Switch To IA-32e Mode Success~!!" );
+	kPrintString( 0, 12, "IA-32e C Language Kernel Start..............[Pass]" );
 
-    kPrintString( 0, 13, "GDT Initialize And Switch For IA-32e Mode...[    ]" );
-    kInitializeGDTTableAndTSS();
-    kLoadGDTR( GDTR_STARTADDRESS );
-    kPrintString( 45, 13, "Pass" );
-    
-    kPrintString( 0, 14, "TSS Segment Load............................[    ]" );
-    kLoadTR( GDT_TSSSEGMENT );
-    kPrintString( 45, 14, "Pass" );
-    
-    kPrintString( 0, 15, "IDT Initialize..............................[    ]" );
-    kInitializeIDTTables();    
-    kLoadIDTR( IDTR_STARTADDRESS );
-    kPrintString( 45, 15, "Pass" );
-	kPrintString(0,15,"Keyboard Activate...........................[    ]");
+	kPrintString( 0, 13, "GDT Initialize And Switch For IA-32e Mode...[    ]" );
+	kInitializeGDTTableAndTSS();
+	kLoadGDTR( GDTR_STARTADDRESS );
+	kPrintString( 45, 13, "Pass" );
+
+	kPrintString( 0, 14, "TSS Segment Load............................[    ]" );
+	kLoadTR( GDT_TSSSEGMENT );
+	kPrintString( 45, 14, "Pass" );
+
+	kPrintString( 0, 15, "IDT Initialize..............................[    ]" );
+	kInitializeIDTTables();    
+	kLoadIDTR( IDTR_STARTADDRESS );
+	kPrintString( 45, 15, "Pass" );
+	kPrintString( 0, 16, "Keyboard Activate And Queue Initialize......[    ]" );
 
 
 
 	// 키보드를 활성화
-	if( kActivateKeyboard() == TRUE )
+	if( kInitializeKeyboard()  == TRUE )
 	{
-		kPrintString(45,15,"Pass");
+		kPrintString(45,16,"Pass");
 		kChangeKeyboardLED(FALSE, FALSE, FALSE);
 	}
 	else{
-		kPrintString(45,15,"Fail");
+		kPrintString(45,16,"Fail");
 		while(1);
 	}
 
-    kPrintString( 0, 16, "This message is printed through the video memory relocated to 0xAB8000" );
+	kPrintString( 0, 17, "This message is printed through the video memory relocated to 0xAB8000" );
 
-    kPrintString( 0, 17, "Test the Read-only Page Funtion: Read.......[    ]" );
-    if(ReadTest())
-        kPrintString( 45, 17, "Pass");
-    
-    kPrintString( 0, 18, "Test the Read-only Page Funtion: Write......[    ]" );
-    if(WriteTest())
-        kPrintString( 45, 18, "Pass");
+	kPrintString( 0, 18, "Test the Read-only Page Funtion: Read.......[    ]" );
+	if(ReadTest())
+		kPrintString( 45, 18, "Pass");
 
-	kPrintString(0,19,"PIC Controller And Interrupt Initialize.....[    ]");
+	kPrintString( 0, 19, "Test the Read-only Page Funtion: Write......[    ]" );
+	if(WriteTest())
+		kPrintString( 45, 19, "Pass");
+
+	kPrintString(0,20,"PIC Controller And Interrupt Initialize.....[    ]");
 	// PIC 컨트롤러 초기화 및 모든 인터럽트 활성화
 	kInitializePIC();
 	kMaskPICInterrupt(0);
 	kEnableInterrupt();
-	kPrintString(45,19,"Pass");
+	kPrintString(45,20,"Pass");
 
 	while(1)
 	{
-		// 출력 버퍼(포트 0x60)가 차 있으면 스캔 코드를 읽을 수 있음
-		if(kIsOutputBufferFull() == TRUE)
+		// 키 큐에 데이터가 있음녀 키를 처
+		if( kGetKeyFromKeyQueue( &stData ) == TRUE)
 		{
-			bTemp = kGetKeyboardScanCode();
 
-			// 스캔 코드를 ASCII 코드로 변환하는 함수를 호출하여 ASCII 코드와
-			// 눌림 또는 떨어짐 정보를 반환
-			if( kConvertScanCodeToASCIICode(bTemp , &(vcTemp[0]), &bFlags) == TRUE)
+			// 키가 눌러졌으면 키의 ASCII 코드 값을 화면에 출력
+			if( stData.bFlags & KEY_FLAGS_DOWN)
 			{
-				// 키가 눌러졌으면 키의 ASCII 코드 값을 화면에 출력
-				if(bFlags & KEY_FLAGS_DOWN)
+				vcTemp[0] = stData.bASCIICode;
+				kPrintString(i++,21,vcTemp);
+				//0이 입력되면 변수를 0으로 나누어 Divide Error 예외(벡터 0번)를 발생시킴
+				if(vcTemp[0] == '0')
 				{
-					kPrintString(i++,20,vcTemp);
-					//0이 입력되면 변수를 0으로 나누어 Divide Error 예외(벡터 0번)를 발생시킴
-					if(vcTemp[0] == '0')
-					{
-						//아래 코드를 수행하면 Divide Error 예외가 발생하여
-						//커널의 임시 핸들러가 수행됨
-						bTemp = bTemp / 0;
-					}
+					//아래 코드를 수행하면 Divide Error 예외가 발생하여
+					//커널의 임시 핸들러가 수행됨
+					bTemp = bTemp / 0;
 				}
 			}
 		}
+
 	}
 
-    
+
 }
 
 // 문자열 출력 함수
 void kPrintString( int iX, int iY, const char* pcString )
 {
-    CHARACTER* pstScreen = ( CHARACTER* ) 0xAB8000;
-    int i;
+	CHARACTER* pstScreen = ( CHARACTER* ) 0xAB8000;
+	int i;
 
-    pstScreen += ( iY * 80  ) + iX;
-    for( i = 0 ; pcString[ i ] != 0 ; i++ )
-    {
-        pstScreen[ i ].bCharacter = pcString[ i ];
-    }
+	pstScreen += ( iY * 80  ) + iX;
+	for( i = 0 ; pcString[ i ] != 0 ; i++ )
+	{
+		pstScreen[ i ].bCharacter = pcString[ i ];
+	}
 }
 
 // Read_Only  Read 기능을 테스트하는 함수
 BOOL ReadTest( void )
 {
-    DWORD testValue = 0xFF;
+	DWORD testValue = 0xFF;
 
-    DWORD* pdwCurrentAddress = ( DWORD* ) 0x1ff000;
-    
-    testValue = *pdwCurrentAddress;
+	DWORD* pdwCurrentAddress = ( DWORD* ) 0x1ff000;
 
-    if(testValue != 0xFF)
-    {
-        return TRUE;
-    }
-    return FALSE;
+	testValue = *pdwCurrentAddress;
+
+	if(testValue != 0xFF)
+	{
+		return TRUE;
+	}
+	return FALSE;
 }
 
 // Read_Only  Write 기능을 테스트하는 함수
 BOOL WriteTest( void )
 {
-    DWORD* pdwCurrentAddress = ( DWORD* ) 0x1ff000;
-    *pdwCurrentAddress =0x12345678;
-    
-    if(*pdwCurrentAddress !=0x12345678){
-        return FALSE;
-    }
-    return TRUE;
+	DWORD* pdwCurrentAddress = ( DWORD* ) 0x1ff000;
+	*pdwCurrentAddress =0x12345678;
+
+	if(*pdwCurrentAddress !=0x12345678){
+		return FALSE;
+	}
+	return TRUE;
 }
--- 02.Kernel64/Source/Utility.c
+++ 02.Kernel64/Source/Utility.c
@@ -7,6 +7,7 @@
  */
 
 #include "Utility.h"
+#include "AssemblyUtility.h"
 
 /**
  *  메모리를 특정 값으로 채움
@@ -14,7 +15,7 @@
 void kMemSet( void* pvDestination, BYTE bData, int iSize )
 {
     int i;
-    
+
     for( i = 0 ; i < iSize ; i++ )
     {
         ( ( char* ) pvDestination )[ i ] = bData;
@@ -27,12 +28,12 @@ void kMemSet( void* pvDestination, BYTE bData, int iSize )
 int kMemCpy( void* pvDestination, const void* pvSource, int iSize )
 {
     int i;
-    
+
     for( i = 0 ; i < iSize ; i++ )
     {
         ( ( char* ) pvDestination )[ i ] = ( ( char* ) pvSource )[ i ];
     }
-    
+
     return iSize;
 }
 
@@ -43,7 +44,7 @@ int kMemCmp( const void* pvDestination, const void* pvSource, int iSize )
 {
     int i;
     char cTemp;
-    
+
     for( i = 0 ; i < iSize ; i++ )
     {
         cTemp = ( ( char* ) pvDestination )[ i ] - ( ( char* ) pvSource )[ i ];
@@ -54,3 +55,30 @@ int kMemCmp( const void* pvDestination, const void* pvSource, int iSize )
     }
     return 0;
 }
+
+/**
+ *  RFLAGS 레지스터의 인터럽트 플래그를 변경하고 이전 인터럽트 플래그의 상태를 반환
+ */
+BOOL kSetInterruptFlag( BOOL bEnableInterrupt )
+{
+    QWORD qwRFLAGS;
+
+    // 이전의 RFLAGS 레지스터 값을 읽은 뒤에 인터럽트 가능/불가 처리
+    qwRFLAGS = kReadRFLAGS();
+    if( bEnableInterrupt == TRUE )
+    {
+        kEnableInterrupt();
+    }
+    else
+    {
+        kDisableInterrupt();
+    }
+
+    // 이전 RFLAGS 레지스터의 IF 비트(비트 9)를 확인하여 이전의 인터럽트 상태를 반환
+    if( qwRFLAGS & 0x0200 )
+    {
+        return TRUE;
+    }
+    return FALSE;
+}
+
