03b0c6e0a19c43527b21d19d3e7ed0f4a3cddddb
hanjoo <johanjoo@naver.com>
2018-10-21 06:58:26 -0700

    GDT, IDT 테이블,TSS 세그먼트 추가해 인터럽트 대비

--- 02.Kernel64/Source/Main.c
+++ 02.Kernel64/Source/Main.c
@@ -1,5 +1,6 @@
 #include "Types.h"
 #include "Keyboard.h"
+#include "Descriptor.h"
 
 // 함수 선언
 void kPrintString( int iX, int iY, const char* pcString );
@@ -16,28 +17,43 @@ void Main( void )
 
     kPrintString( 0, 11, "Switch To IA-32e Mode Success~!!" );
     kPrintString( 0, 12, "IA-32e C Language Kernel Start..............[Pass]" );
-	kPrintString(0,13,"Keyboard Activate...........................[    ]");
+
+    kPrintString( 0, 13, "GDT Initialize And Switch For IA-32e Mode...[    ]" );
+    kInitializeGDTTableAndTSS();
+    kLoadGDTR( GDTR_STARTADDRESS );
+    kPrintString( 45, 13, "Pass" );
+    
+    kPrintString( 0, 14, "TSS Segment Load............................[    ]" );
+    kLoadTR( GDT_TSSSEGMENT );
+    kPrintString( 45, 14, "Pass" );
+    
+    kPrintString( 0, 15, "IDT Initialize..............................[    ]" );
+    kInitializeIDTTables();    
+    kLoadIDTR( IDTR_STARTADDRESS );
+    kPrintString( 45, 15, "Pass" );
+	kPrintString(0,15,"Keyboard Activate...........................[    ]");
 
 	// 키보드를 활성화
 	if( kActivateKeyboard() == TRUE )
 	{
-		kPrintString(45,13,"Pass");
+		kPrintString(45,15,"Pass");
 		kChangeKeyboardLED(FALSE, FALSE, FALSE);
 	}
 	else{
-		kPrintString(45,13,"Fail");
+		kPrintString(45,15,"Fail");
 		while(1);
 	}
 
-    kPrintString( 0, 14, "This message is printed through the video memory relocated to 0xAB8000" );
+    kPrintString( 0, 16, "This message is printed through the video memory relocated to 0xAB8000" );
 
-    kPrintString( 0, 15, "Test the Read-only Page Funtion: Read.......[    ]" );
+    kPrintString( 0, 17, "Test the Read-only Page Funtion: Read.......[    ]" );
     if(ReadTest())
-        kPrintString( 45, 15, "Pass");
+        kPrintString( 45, 17, "Pass");
     
-    kPrintString( 0, 16, "Test the Read-only Page Funtion: Write......[    ]" );
+    kPrintString( 0, 18, "Test the Read-only Page Funtion: Write......[    ]" );
     if(WriteTest())
-        kPrintString( 45, 16, "Pass");
+        kPrintString( 45, 18, "Pass");
+
 	while(1)
 	{
 		// 출력 버퍼(포트 0x60)가 차 있으면 스캔 코드를 읽을 수 있음
@@ -52,7 +68,14 @@ void Main( void )
 				// 키가 눌러졌으면 키의 ASCII 코드 값을 화면에 출력
 				if(bFlags & KEY_FLAGS_DOWN)
 				{
-					kPrintString(i++,17,vcTemp);
+					kPrintString(i++,19,vcTemp);
+					//0이 입력되면 변수를 0으로 나누어 Divide Error 예외(벡터 0번)를 발생시킴
+					if(vcTemp[0] == '0')
+					{
+						//아래 코드를 수행하면 Divide Error 예외가 발생하여
+						//커널의 임시 핸들러가 수행됨
+						bTemp = bTemp / 0;
+					}
 				}
 			}
 		}
